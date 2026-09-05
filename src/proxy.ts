eb78b0b1e6bcc2d5314a39fe2f30e8044a4fa689
varunn_ranaa <varunrana1777@gmail.com>
2026-09-05 10:21:23 +0530

    feat: add proxy middleware for authentication and redirection

---
+++ src/proxy.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+export { default } from "next-auth"
+import { getToken } from 'next-auth/jwt'
+ 
+
+export async function proxy(request: NextRequest) {
+    const token = await getToken({req : request})
+    const { pathname } = request.nextUrl;
+
+  const isPublicPath =
+    pathname === '/' ||
+    pathname.startsWith('/login') ||
+    pathname.startsWith('/signup') ||
+    pathname.startsWith('/verify');
+
+  if (token && isPublicPath) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+
+  if (!token && pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+ 
+export const config = {
+  matcher: [
+    '/login',
+    '/signup',
+    '/',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+}
