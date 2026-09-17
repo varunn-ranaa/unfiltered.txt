a42cddeb4f72f3a74e24153c6dd16745dd204665
varunn_ranaa <varunrana1777@gmail.com>
2026-09-17 14:51:20 +0530

    refactor: clean up proxy function by removing unnecessary semicolons and improving code readability

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,29 +1,27 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-export { default } from "next-auth"
 import { getToken } from 'next-auth/jwt'
- 
 
 export async function proxy(request: NextRequest) {
-    const token = await getToken({req : request, secret: process.env.NEXTAUTH_SECRET})
-    const { pathname } = request.nextUrl;
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET })
+  const { pathname } = request.nextUrl
 
   const isAuthOnlyPath =
     pathname.startsWith('/login') ||
     pathname.startsWith('/signup') ||
-    pathname.startsWith('/verify');
+    pathname.startsWith('/verify')
 
   if (token && isAuthOnlyPath) {
-    return NextResponse.redirect(new URL('/dashboard', request.url));
+    return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
   if (!token && pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
- 
+
 export const config = {
   matcher: [
     '/login',
