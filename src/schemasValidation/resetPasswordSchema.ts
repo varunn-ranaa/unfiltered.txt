1301269d76b678cb5901c8d87418a77e5a6a39f3
varunn_ranaa <varunrana1777@gmail.com>
2026-09-22 20:17:30 +0530

    refactor: update reset password validation schema to include code and new password fields

--- src/schemasValidation/resetPasswordSchema.ts
+++ src/schemasValidation/resetPasswordSchema.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'; 
 
 
-export const resetAPasswordValidation = z.object({
-    password: z
-    .string()
-    .min(6, { message: "Password must be at least 6 characters long" })
-    .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: "Password must contain at least one special character" })
-}); 
+export const resetPasswordValidation =  z.object(
+      {
+              code: z.string().length(6, `Code must be 6 digits`),
+              newpassword: z.string()
+                  .min(6, { message: "Password must be at least 6 characters long" })
+                  .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: "Password must contain at least one special character" }),
+      })
